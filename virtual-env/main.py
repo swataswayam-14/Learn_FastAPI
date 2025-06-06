f5b54b963c89f452228af7f7e06c58dea28ef5c6
Swata Swayam Dash <swataswayamdash@gmail.com>
2025-06-06 16:24:48 +0530

    starting a fast api server

--- virtual-env/main.py
+++ virtual-env/main.py
@@ -1,15 +1,22 @@
-print("Hello World")
+from fastapi import FastAPI #FastAPI is a class that inherits directly from Starlette
 
-# source .venv/bin/activate : activating a virtual environment
-# checking : which python
-# desctivating the virtual env : deactivate , we can use this command wherever we are , when activating the virtual env for another project , we have to go to that directory and : ".venv/bin/activate"
+app = FastAPI() # app is a FastAPI instance
 
-# Activating a virtual environment adds its path .venv/bin (on Linux and macOS) 
-# before activating the PATH variable looked like : "/usr/bin:/bin:/usr/sbin:/sbin"
-#that means the system would look for programs in : "/usr/bin:/bin:/usr/sbin:/sbin"
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-# after activating venv , the PATH variable would be : "/home/user/code/awesome-project/.venv/bin:/usr/bin:/bin:/usr/sbin:/sbin"
-# So, when you type python in the terminal, the system will find the Python program in
-# : "/home/user/code/awesome-project/.venv/bin/python" and use that one
+#Path : (endpoint / route)
+# "Path" here refers to the last part of the URL starting from the first /.
+# URL : https://example.com/items/foo, path: "/items/foo"
 
-# locking : being able to run your project in production exactly the same as in your computer while developing.
+
+# http://127.0.0.1:8000/
+# http://127.0.0.1:8000/docs : interactive API doc by Swagger UI
+# http://127.0.0.1:8000/redoc : alternative automatic documentation (provided by ReDoc
+# FastAPI automatically generates a JSON (schema) with the descriptions of all your API : http://127.0.0.1:8000/openapi.json
+
+
+#in OPEN API (HTTP Methods (GET, POST, PUT, DELETE)) are called Operations
+
+#@something : is a decorator , tells FastAPI that the function below corresponds to the path / with an operation get/put etc
