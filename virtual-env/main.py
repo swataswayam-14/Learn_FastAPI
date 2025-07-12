13d82fb011522c8a4a63bd77910a62669ec5d94e
Swata Swayam Dash <swataswayamdash@gmail.com>
2025-07-12 08:55:50 +0530

    implement query parameters

--- virtual-env/main.py
+++ virtual-env/main.py
@@ -12,9 +12,9 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int): # with type declaration , fast api gives us automatic request parsing
-    return {"item_id": item_id}
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int): # with type declaration , fast api gives us automatic request parsing
+#     return {"item_id": item_id}
 
 # http://127.0.0.1:8000/redoc
 # http://127.0.0.1:8000/docs
@@ -37,6 +37,43 @@ async def get_model(model_name: ModelName):
     if model_name.value == "lenet": 
         return {"model_name": model_name, "message": "leCNN all the images"}
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip+limit]
+
+@app.get("/items/{item_id}") #here item_id is a path parameter and q is a query parameter
+async def read_item(item_id: str, q: str|None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+@app.get("/things/{thing_id}")
+async def read_things(thing_id: str, q: str | None = None , short: bool = False):
+    thing = {"thing_id": thing_id}
+    if q:
+        thing.update({ "q": q })
+    if not short: 
+        thing.update({ "description": "This is an amazint thing" })
+    return thing
+
+#multiple path and query parameters
+
+@app.get("/toms/{tom_id}/cat/{cat_id}")
+async def read_kitten_profile(
+    tom_id: int, cat_id: int, q:str | None = None, short: bool = False
+):
+    item = {"cat_id": cat_id, "owner_id": tom_id}
+    if q: 
+        item.update({ "q": q })
+    if not short: 
+        item.update(
+            {"description" :"this is a long description"}
+        )
+    return item
 
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
