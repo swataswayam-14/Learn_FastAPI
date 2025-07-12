328369c9773cdee115dd887a139087e73156123f
Swata Swayam Dash <swataswayamdash@gmail.com>
2025-07-12 12:42:31 +0530

    implement query parameters and string validations

--- virtual-env/main.py
+++ virtual-env/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI , Query
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, AfterValidator
+from typing import Annotated
+import random
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
@@ -12,6 +14,17 @@ class Item(BaseModel):
     description: str | None = None
     price: float
     tax: float | None = None
+
+data = {
+    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
+    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
+    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",    
+}
+
+def check_valid_id(id: str):
+    if not id.startswith(("isbn-", "imdb-")):
+        raise ValueError('Invalid ID format, it must start with isbn- or imdb-')
+    return id
 
 app = FastAPI() 
 
@@ -119,3 +132,111 @@ async def read_kitten_profile(
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+@app.get("/jinsa/")
+async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
+    results = {
+        "items":[
+            {"item_id": "foo"},
+            {"item_id": "bar"}
+        ]
+    }
+    if q: 
+        results.update({"q": q})
+    return results
+
+# q: str | None = None ===>  q: Annonated[str|None] = None
+
+
+#older versions had Query as the default value of the parameter, instead of putting it in Annotated
+
+@app.get("/pariba")
+async def get_pariba(q: str | None = Query(default=None, max_length=50)):
+    results = {
+        "pariba": [
+            {"baigana": "10kg"},
+            {"tomato": "20kg"}
+        ]
+    }
+    if q:
+        results.update({ "q": q})
+    return results
+
+@app.get("/rook")
+async def more_parameters (q: Annotated[str | None, Query(min_length=3, max_length= 50)] = None):
+    results = {"item1": "bhendi", "item2": "alu"}
+    if q:
+        results.update({"q":q})
+    return results
+
+@app.get("/rookie")
+async def regular_expressions (q: Annotated[str | None, Query(min_length=3, max_length= 50, pattern="^fixedquery$")] = None):
+    results = {"item1": "bhendi", "item2": "alu"}
+    if q:
+        results.update({"q":q})
+    return results
+
+#pattern was previously called regex , now its deprecated
+
+@app.get("/horse")
+async def regular_expressions (q: Annotated[str | None, Query(min_length=3, max_length= 50)] = "fixedquery"):
+    results = {"item1": "bhendi", "item2": "alu"}
+    if q:
+        results.update({"q":q})
+    return results
+
+# Query parameter list / multiple values
+#here we need to explicitly use Query, otherwise it would be interpreted as a Request Body
+@app.get("/multiplequery")
+async def read_item(q: Annotated[list[str] | None , Query()] = None):
+    query_items = {"q": q}
+    return query_items
+
+@app.get("/mwd")
+async def read_item(q: Annotated[list[str] | None , Query()] = ["foo", "bar"]):
+    query_items = {"q": q}
+    return query_items
+
+#declaring more metadata
+@app.get("/meta")
+async def read_item(q: Annotated[str | None, Query(title="This is a metadata of what the query is", description="query is querying")]=None):
+    if q:
+        return {"item_id": "item1", "q": q}
+    return {"item_id": "foo"}
+
+#alias parameter
+#item-query is not a valid Python variable name, but we want it so....
+@app.get("/alias/example")
+async def read_items(q: Annotated[str|None, Query(alias="item-query")] = None):
+    results = {"items": 1}
+    if q:
+        results.update({"q": q})
+    return results
+
+#if a parameter is depricaiated but used my clients , to show that its depricated: 
+
+@app.get("/depriciated/param/example")
+async def read_items( q: Annotated[str|None, Query(alias="item-query", title="Query string", description="query string for searching item in the DB",min_length=3, max_length=50, deprecated=True)] = None):
+    results = {"items": [{"item1": "1"}, {"item2": "2"}]}
+    if q:
+        results.update({"q":q})
+    return results
+
+# Exclude parameters from OpenAPI
+
+@app.get("/random")
+async def read_items(hidden_query: Annotated[str | None, Query(include_in_schema=False)] = None):
+    if hidden_query:
+        return {"hidden_querry": hidden_query}
+    else:
+        return {"hidden_query": "Not found"}
+    
+
+# Custom Validation
+@app.get("/custom/validator/example")
+async def read_items( id: Annotated[str | None, AfterValidator(check_valid_id)] = None):
+    if id: 
+        item = data.get(id)
+    else:
+        id, item = random.choice(list(data.items()))
+    return {"id":id, "name": item}
